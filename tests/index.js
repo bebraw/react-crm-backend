2445b43280b8b2b7a383bfcedf05e4c52e4797fa
Juho Vepsalainen <bebraw@gmail.com>
2015-01-08 15:51:56 +0200

    Set up a test showing how schema validation can fail

---
+++ tests/index.js
@@ -0,0 +1,28 @@
+'use strict';
+var url = require('url');
+
+var axios = require('axios');
+
+var apikey = require('../config/').apikey;
+
+
+tests();
+
+function tests() {
+    var root = 'http://localhost:3000';
+
+    testPostInvoice(root);
+}
+
+function testPostInvoice(root) {
+    // this is missing post data on purpose
+    axios.post(url.resolve(root, '/v1/invoices'), {}, {
+        headers: {
+            'x-auth-token': apikey
+        }
+    }).then(function(res) {
+        console.log('ok, res');
+    }, function(err) {
+        console.error(err);
+    });
+}
