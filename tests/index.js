68c30a1d3c20894df0a9aebc739b71c55bead47c
Juho Vepsalainen <bebraw@gmail.com>
2015-01-12 11:44:54 +0200

    Improve testing architecture
    Now it will run the server automatically when hitting `npm test`. It
    will also create a test database (erased for each test).

--- tests/index.js
+++ tests/index.js
@@ -1,35 +1,57 @@
+/* global -Promise */
 'use strict';
 var url = require('url');
 
 var axios = require('axios');
+var Promise = require('bluebird');
+
+var config = require('../config');
+var models = require('../models')(config.database.test);
+var server = require('../server');
 
 
 tests();
 
 function tests() {
-    var root = 'http://localhost:3000';
+    var port = 1351;
+    var root = 'http://localhost:' + port;
+    var testcases = [testPostInvoice];
 
-    authenticate(root).then(testPostInvoice.bind(null, root), function(err) {
-        console.error('failed to authenticate', err);
+    testcases = testcases.map(function(fn) {
+        return Promise.using(models.sequelize.sync({
+            force: true
+        }), function() {
+            return fn(root);
+        });
+    });
+
+    server(function(app) {
+        var s = app.listen(port);
+
+        Promise.all(testcases).catch(function(err) {
+            console.error(err);
+        }).finally(function() {
+            s.close();
+        });
+    });
+}
+
+function testPostInvoice(root) {
+    return new Promise(function(resolve, reject) {
+        authenticate(root).then(function(res) {
+            var u = url.resolve(root, '/v1/invoices');
+            var token = res.data && res.data.token;
+
+            // this is missing post data on purpose
+            axios.post(u, {}, {
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                }
+            }).then(resolve).catch(reject);
+        });
     });
 }
 
 function authenticate(root) {
     return axios.post(url.resolve(root, 'authenticate'));
-}
-
-function testPostInvoice(root, res) {
-    var u = url.resolve(root, '/v1/invoices');
-    var token = res.data && res.data.token;
-
-    // this is missing post data on purpose
-    axios.post(u, {}, {
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    }).then(function(res) {
-        console.log('ok', res);
-    }, function(err) {
-        console.error(err);
-    });
 }
