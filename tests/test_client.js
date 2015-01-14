98b7555af425079b48963dca320d8203c2384f24
Juho Vepsalainen <bebraw@gmail.com>
2015-01-14 10:20:50 +0200

    Add TODOs

--- tests/test_client.js
+++ tests/test_client.js
@@ -26,17 +26,27 @@ exports.postInvalid = function(assert, client) {
 
 exports.postValid = function(assert, client) {
     // TODO: generate a valid client based on schema
+    // client.clients.post.parameters.schema / description etc.
+    // attach post.parameters.schema + resolve ref
+    // pass schema to generator + attach generated object to body
     return client.clients.post({}).then(function() {
+        // make sure id was received (post.responses.200.schema)
         assert(true, 'Posted client as expected');
+
+        // perform get with id now
     }).catch(function(err) {
         assert(false, 'Failed to post client', err);
     });
 };
 
 exports.put = function(assert, client) {
+    // TODO: post, put using same id, get using id + validate
     return client.clients.put().then(function() {
         assert(false, 'Updated client even though shouldn\'t');
     }).catch(function() {
         assert(true, 'Failed to update client as expected');
     });
 };
+
+// TODO: test GET pagination
+// TODO: test GET sortBy
