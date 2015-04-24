92d768b86acb589363a1b2f0586064ee50b3f518
Juho Vepsalainen <bebraw@gmail.com>
2015-04-24 13:35:20 +0300

    Trigger GET test for approved invoices

--- tests/test_approvedinvoice.js
+++ tests/test_approvedinvoice.js
@@ -7,24 +7,11 @@
     POST invoice (ok/fail)
 */
 
-// TODO: connect + trigger tests
-module.exports = function(assert, client) {
-    var resource = client.approvedinvoices;
+var restTemplate = require('./rest_template');
 
-    return {
-        get: function() {
-            return resource.get().then(function(res) {
-                assert(res.data.length === 0, 'Failed to get invoices as expected');
-            }).catch(function() {
-                assert(true, 'Failed to get invoices as expected');
-            });
-        },
-        post: function() {
-            return resource.post().then(function() {
-                assert(false, 'Posted invoice even though shouldn\'t');
-            }).catch(function() {
-                assert(true, 'Failed to post invoice as expected');
-            });
-        }
-    };
-};
+module.exports = restTemplate.execute('approvedinvoice', [
+    restTemplate.getTests
+]);
+
+// the problem is that the other tests rely either or PUT/POST. these will be
+// implemented differently in this case due to transition logic
