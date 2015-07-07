8729595668e27fc4864ea3eb0747221e3b58e0fd
Juho Vepsalainen <bebraw@gmail.com>
2015-07-07 13:40:04 +0300

    Connect to database before testing and nuke db before each

--- tests/test_invoice.js
+++ tests/test_invoice.js
@@ -1,20 +1,29 @@
 'use strict';
 var assert = require('assert');
 
+var config = require('../config');
+
+config.database.test.logging = noop;
+
+var models = require('../models')(config.database.test);
+var Invoice = models.Invoice;
+
 describe('Invoice', function() {
   beforeEach(function(done) {
-    // TODO: connect to db and provide a handle to it
-    done();
+    // nuke possible db
+    models.sequelize.sync({force: true}).finally(done);
   });
 
   it('should be able to create an invoice', function(done) {
     // TODO
     console.log('should be able to create an invoice');
 
-    assert(false);
-
-    done();
+    Invoice.create().then(function(result) {
+      done();
+    }).catch(done);
   });
 
   // TODO: test transformations etc.
 });
+
+function noop() {}
