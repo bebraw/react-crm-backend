ca3dd8cd3a2f3366a7d2f16cae26b1b8d9ff55b4
Juho Vepsalainen <bebraw@gmail.com>
2015-07-13 14:21:27 +0300

    Separate invoice creation

--- tests/test_invoice.js
+++ tests/test_invoice.js
@@ -32,34 +32,15 @@ describe('Invoice REST API', function() {
   });
 
   it('should be able to POST an invoice', function(done) {
-    var resource = this.resource;
-    var postSchema = resource.post.parameters[0].schema;
+    createInvoice(this.client, this.schema, this.resource, function(err) {
+      if(err) {
+        return done(err);
+      }
 
-    createClientAndUser(function(client, user) {
-      // generate proper receiver/sender ids
-      generateDependencies(this.client, this.schema, postSchema).then(function(d) {
-        d.push({
-          field: 'receiver',
-          value: client.id
-        });
-        d.push({
-          field: 'sender',
-          value: user.id
-        });
-        d.push({
-          field: 'due',
-          value: new Date()
-        });
+      assert(true, 'Posted ' + resourceName + ' as expected');
 
-        resource.post(
-          patchParameters(getParameters(postSchema), d)
-        ).then(function() {
-          assert(true, 'Posted ' + resourceName + ' as expected');
-
-          done();
-        }).catch(done);
-      }).catch(done);
-    }.bind(this));
+      done();
+    });
   });
 
   it('should be able to GET an invoice', function(done) {
@@ -71,8 +52,11 @@ describe('Invoice REST API', function() {
   });
 
   it('should be able to PUT an invoice', function(done) {
-    // TODO
-    done();
+    this.resource.put().then(function() {
+      assert(false, 'Updated ' + resourceName + ' even though shouldn\'t');
+    }).catch(function() {
+      assert(true, 'Failed to update ' + resourceName + ' as expected');
+    }).finally(done);
   });
 
   it('should be able to approve an invoice', function(done) {
@@ -85,3 +69,30 @@ describe('Invoice REST API', function() {
     done();
   });
 });
+
+function createInvoice(apiClient, schema, resource, cb) {
+  var postSchema = resource.post.parameters[0].schema;
+
+  createClientAndUser(function(client, user) {
+    generateDependencies(apiClient, schema, postSchema).then(function(d) {
+      d.push({
+        field: 'receiver',
+        value: client.id
+      });
+      d.push({
+        field: 'sender',
+        value: user.id
+      });
+      d.push({
+        field: 'due',
+        value: new Date()
+      });
+
+      resource.post(
+        patchParameters(getParameters(postSchema), d)
+      ).then(function(invoice) {
+        cb(null, invoice);
+      }).catch(cb);
+    }).catch(cb);
+  });
+}
