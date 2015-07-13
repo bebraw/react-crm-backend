5d65bc4a7dbd8eb9ede8854b111521671db136ed
Juho Vepsalainen <bebraw@gmail.com>
2015-07-13 15:42:46 +0300

    Add invoice status transition logic

--- routes/invoice.js
+++ routes/invoice.js
@@ -5,11 +5,92 @@ var templates = require('./templates');
 
 
 module.exports = function(imports) {
-    var model = imports.models.Invoice;
+  var model = imports.models.Invoice;
 
-    return swaggerify('invoice', {
-        get: templates.get(model),
-        post: templates.post(model),
-        put: templates.put(model),
-    });
+  return swaggerify('invoice', {
+    get: templates.get(model),
+    post: templates.post(model),
+    put: put(model),
+  });
 };
+
+function put(model) {
+  return function(req, res) {
+    var body = req.swagger.params.body.value;
+    var bodyId = body.id;
+
+    delete body.id;
+
+    // TODO: check current status before operating
+
+    model.findOne({
+      id: bodyId
+    }).then(function(result) {
+      var dataValues = result.dataValues;
+
+      if(dataValues.status === 'paid' && body.status !== 'paid') {
+        return res.status(403).json({
+          message: 'Changing paid status is not allowed',
+          errors: [],
+          warnings: []
+        });
+      }
+
+      if(dataValues.status === 'pending' && body.status === 'paid') {
+        return res.status(403).json({
+          message: 'Changing pending status to paid is not allowed',
+          errors: [],
+          warnings: []
+        });
+      }
+
+      if(dataValues.status === 'approved' && body.status === 'pending') {
+        return res.status(403).json({
+          message: 'Changing approved status to pending is not allowed',
+          errors: [],
+          warnings: []
+        });
+      }
+
+      model.update(body, {
+        where: {
+          id: bodyId
+        }
+      }).then(function(ids) {
+        var id = ids[0];
+
+        if(id) {
+          model.findOne({
+            id: id
+          }).then(function(result) {
+            res.json(result.dataValues);
+          }).catch(function(err) {
+            res.status(403).json({
+              message: err.message,
+              errors: err.errors,
+              warnings: []
+            });
+          });
+        }
+        else {
+          // TODO: specify this case better
+          res.status(403).json({
+            message: 'NOT_FOUND'
+          });
+        }
+      }).catch(function(err) {
+        res.status(403).json({
+          message: err.message,
+          errors: err.errors,
+          warnings: []
+        });
+      });
+    }).catch(function(err) {
+      res.status(403).json({
+        message: err.message,
+        errors: err.errors,
+        warnings: []
+      });
+    });
+  };
+}
